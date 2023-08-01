315f5da224e32b90111e96f9e78de00257f6a04f
Michael Großklaus <mail@mgrossklaus.de>
2023-08-01 09:58:49 +0200

    Updated dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,22 @@
 module.exports = {
 	root: true,
 	parser: "@typescript-eslint/parser",
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-	plugins: ["svelte3", "@typescript-eslint"],
-	ignorePatterns: ["*.cjs"],
-	overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
-	settings: {
-		"svelte3/typescript": () => require("typescript"),
-	},
+	extends: ["plugin:svelte/recommended", "plugin:@typescript-eslint/recommended", "prettier"],
 	parserOptions: {
-		sourceType: "module",
-		ecmaVersion: 2020,
+		project: "tsconfig.json",
+		extraFileExtensions: [".svelte"],
 	},
+	overrides: [
+		{
+			files: ["*.svelte"],
+			parser: "svelte-eslint-parser",
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+			},
+		},
+	],
+	plugins: ["@typescript-eslint"],
+	ignorePatterns: ["*.cjs"],
 	env: {
 		browser: true,
 		es2017: true,
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
-/** @type {import('vite').UserConfig} */
-const config = {
+export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		port: 3000,
 	},
-};
-
-export default config;
+});
